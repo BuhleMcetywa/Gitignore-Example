85a21d77cbafcbdbdfb0353d131d40a4f5f14be4
BuhleMcetywa <buhlemcetywa07@gmail.com>
2023-11-30 11:16:03 +0200

    Initial Commit

---
+++ Gitignore example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gitignore_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
